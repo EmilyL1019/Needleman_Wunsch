add313a7b689ee9d988aa655813090fb3fed33a8
Emily Light <77697184+EmilyL1019@users.noreply.github.com>
2022-08-21 16:45:57 -0400

    Update alignment.rs

--- alignment.rs
+++ alignment.rs
@@ -20,8 +20,6 @@ fn priv_best_alignment<'a>(score_grid: &'a Vec<i32>,directions: &'a Vec<String>,
     // Keeps track of how many directions the cell has
     let mut hasDiagonal:bool = false;
     let mut hasLeft:bool = false;
-    let mut hasUp:bool = false;
-    // Add to aligned sequences length
     // If the current direction index is D, add the two corresponding characters to the sequence strings    
     if directions[direction_index].contains("D") {
         let copy_aligned_seq1:&mut Vec<Vec<char>> = &mut new_aligned_seq1;
@@ -42,11 +40,16 @@ fn priv_best_alignment<'a>(score_grid: &'a Vec<i32>,directions: &'a Vec<String>,
         hasLeft = true;
         // Check if cell has multiple paths
         if hasDiagonal {
-            copy_aligned_seq1.insert(*aligned_seq_index + 1, vec![]);
-            copy_aligned_seq2.insert(*aligned_seq_index + 1,vec![]);
-            *aligned_seq_index = *aligned_seq_index + 1;
-            copy_aligned_seq1[*aligned_seq_index] = copy_aligned_seq1[*aligned_seq_index - 1 as usize].to_vec();
-            copy_aligned_seq2[*aligned_seq_index] = copy_aligned_seq2[*aligned_seq_index - 1 as usize].to_vec();
+           let new_index = *aligned_seq_index + 1;
+            println!("{}", new_index);
+            while new_index > *aligned_seq_index {    
+                copy_aligned_seq1.insert(*aligned_seq_index + 1, vec![]);
+                copy_aligned_seq2.insert(*aligned_seq_index + 1,vec![]);
+                *aligned_seq_index = *aligned_seq_index + 1;
+                // Copy old vector into new one
+                //??
+
+            }
         }
         copy_aligned_seq1[*aligned_seq_index].insert(0, '_');
         copy_aligned_seq2[*aligned_seq_index].insert(0, seq2_char);
@@ -59,16 +62,22 @@ fn priv_best_alignment<'a>(score_grid: &'a Vec<i32>,directions: &'a Vec<String>,
     if directions[direction_index].contains("U") {
         let copy_aligned_seq1_2:&mut Vec<Vec<char>> = &mut new_aligned_seq1;
         let copy_aligned_seq2_2:&mut Vec<Vec<char>> = &mut new_aligned_seq2;
+        print!("up!\n");
         // Check if cell has multiple paths
         if hasDiagonal || hasLeft {
-            copy_aligned_seq1_2.insert(*aligned_seq_index + 1, vec![]);
-            copy_aligned_seq2_2.insert(*aligned_seq_index + 1,vec![]);
-            *aligned_seq_index = *aligned_seq_index + 1;
-            copy_aligned_seq1_2[*aligned_seq_index] = copy_aligned_seq1_2[*aligned_seq_index - 1 as usize].to_vec();
-            copy_aligned_seq2_2[*aligned_seq_index] = copy_aligned_seq2_2[*aligned_seq_index - 1 as usize].to_vec();
+            let new_index = *aligned_seq_index + 1;
+            println!("{}", new_index);
+            while new_index > *aligned_seq_index {    
+                copy_aligned_seq1_2.insert(*aligned_seq_index + 1, vec![]);
+                copy_aligned_seq2_2.insert(*aligned_seq_index + 1,vec![]);
+                *aligned_seq_index = *aligned_seq_index + 1;
+                // Copy old vector into new one
+                //??
+                
+            }
         }
         copy_aligned_seq1_2[*aligned_seq_index].insert(0, seq1_char);
-        copy_aligned_seq2_2[*aligned_seq_index].insert(0,'_');
+        copy_aligned_seq2_2[*aligned_seq_index].insert(0, '_');    
         // Move to the cell above the current cell
         if (direction_index as i32 - seq2_len) as i32 >= 0 {
             priv_best_alignment(score_grid, directions, cell - seq2_len as i32, seq1, seq2, copy_aligned_seq1_2, copy_aligned_seq2_2, aligned_seq_index);
@@ -89,8 +98,21 @@ fn priv_best_alignment<'a>(score_grid: &'a Vec<i32>,directions: &'a Vec<String>,
 pub fn build_best_alignment<'a>(score_grid: &'a Vec<i32>,directions: &'a Vec<String>, cell: i32, seq1: &'a mut String, seq2: &'a mut String) -> (Vec<String>, Vec<String>){
     let mut aligned_sequence1:Vec<Vec<char>> = vec![[].to_vec()];
     let mut aligned_sequence2:Vec<Vec<char>> = vec![[].to_vec()];
+    let mut str_aligned_seq1: Vec<String> = Vec::new();
+    let mut str_aligned_seq2: Vec<String> = Vec::new();
     let mut index:usize = 0;
-    return priv_best_alignment(score_grid, directions, cell, seq1, seq2, &mut aligned_sequence1, &mut aligned_sequence2, &mut index);
+    (str_aligned_seq1, str_aligned_seq2) = priv_best_alignment(score_grid, directions, cell, seq1, seq2, &mut aligned_sequence1, &mut aligned_sequence2, &mut index);
+    for i in 0..(aligned_sequence1.len() - 1) {
+        let mut j = str_aligned_seq1[i].len();
+        while j < str_aligned_seq1[i].len() {
+            let char1 = str_aligned_seq1[0].chars().nth(j).unwrap();
+            let char2 = str_aligned_seq2[0].chars().nth(j).unwrap();
+            str_aligned_seq1[i].insert(j, char1);
+            str_aligned_seq2[i].insert(j, char2);
+            j = j + 1;
+        }
+    }
+    return (str_aligned_seq1, str_aligned_seq2);
 }
 
 pub fn print_alignments(str_aligned_seq1: Vec<String>, str_aligned_seq2: Vec<String>) {
